8db57954ae9c204647d8762cfae14a52dfbdf6eb
Christian Jimenez <cajt@outlook.com>
2024-09-23 10:27:23 -0800

    Exercise 6

--- ex05.js
+++ ex05.js
@@ -1,7 +1,17 @@
-// Exercise 5: Write a function named 'groupByCategory' that takes an array of product objects and returns an object where the keys are categories and the values are arrays of products in those categories.
+// Exercise 5: Write a function named 'groupByCategory' that takes an array of product objects and 
+//returns an object where the keys are categories and the values are arrays of products in those categories.
 
 function groupByCategory(products) {
-  // your code here
+  let obj ={}
+  for(let i =0; i < products.length; i++){
+    if(obj.keys != 1){
+      obj[products[i].category]
+    }else if(!products[i].category in obj.keys){
+      obj[products[i].category]
+    }
+    obj[products[i].category]+=[(products[i].name +" "+products[i].category)]
+  }
+  console.log(obj)
 }
 
 console.log(groupByCategory([{ name: 'Apple', category: 'Fruit' }, { name: 'Carrot', category: 'Vegetable' }, { name: 'Banana', category: 'Fruit' }])) // { Fruit: [{ name: 'Apple', category: 'Fruit' }, { name: 'Banana', category: 'Fruit' }], Vegetable: [{ name: 'Carrot', category: 'Vegetable' }] }
--- ex06.js
+++ ex06.js
@@ -1,7 +1,17 @@
-// Exercise 6: Write a function named 'uniqueValues' that takes an array of values and returns a new array with only unique values, removing duplicates.
+// Exercise 6: Write a function named 'uniqueValues' that takes an array of values and 
+//returns a new array with only unique values, removing duplicates.
 
 function uniqueValues(arr) {
-  // your code here
+  let arr_n = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr_n.includes(arr[i])){
+
+    }
+    else{
+      arr_n.push((arr[i]))
+    }
+  }
+  return arr_n
 }
 
 console.log(uniqueValues([1, 2, 2, 3, 4, 4, 5])) // [1, 2, 3, 4, 5]
