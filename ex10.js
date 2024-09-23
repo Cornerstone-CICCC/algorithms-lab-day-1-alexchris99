21b3e8927336cc2326f8a0d4f2ac2fe4521bfd88
Christian Jimenez <cajt@outlook.com>
2024-09-23 11:28:55 -0800

    Excersice 10

--- ex10.js
+++ ex10.js
@@ -1,7 +1,11 @@
-// Exercise 10: Write a function named 'formatDate' that takes a date string (in the format 'YYYY-MM-DD') and returns a formatted date string (e.g., 'January 1, 2022').
+// Exercise 10: Write a function named 'formatDate' that takes a date string (in the format 'YYYY-MM-DD')
+// and returns a formatted date string (e.g., 'January 1, 2022').
 
 function formatDate(dateStr) {
-  // your code here
+  let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+  let date = dateStr.split("-")
+  console.log(months[parseInt(date[1])-1])
+  return months[parseInt(date[1])-1] +" " + date[2]+"," +" " + date[0]
 }
 
 console.log(formatDate('2022-01-01')) // 'January 1, 2022'
