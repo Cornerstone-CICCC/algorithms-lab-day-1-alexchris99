a100281fef445676c1a0fe4fa6a76bb1c641e46a
Christian Jimenez <cajt@outlook.com>
2024-09-23 10:57:24 -0800

    Excersice 9

--- ex09.js
+++ ex09.js
@@ -1,7 +1,9 @@
-// Exercise 9: Write a function named 'flattenArray' that takes an array of nested arrays and returns a single flattened array.
+// Exercise 9: Write a function named 'flattenArray' that takes an array of nested 
+// arrays and returns a single flattened array.
 
 function flattenArray(arr) {
-  // your code here
+  let newArr = arr.flat()
+  return  newArr.flat()
 }
 
 console.log(flattenArray([[1, 2], [3, 4], [5, [6, 7]]])) // [1, 2, 3, 4, 5, 6, 7]
